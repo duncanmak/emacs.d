c017abce7516447d361fdae1652c6d3ad0380b74
Duncan Mak <duncanmak@gmail.com>
2011-08-21 16:43:33 -0400

    Add mac-specific settings.

--- init.el
+++ init.el
@@ -10,6 +10,14 @@
       (normal-top-level-add-to-load-path '("."))
       (normal-top-level-add-subdirs-to-load-path))
 
+;;; Mac specific
+(when (eq system-type 'darwin)
+  (setq mac-option-key-is-meta nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier nil)
+  (set-default-font "Menlo-14"))
+
 (setenv "PATH" (concat (getenv "PATH") ":/home/duncan/bin"))
 
 (require 'recentf)
