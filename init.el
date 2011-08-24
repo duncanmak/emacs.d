a91cd96e4cf6fccf632c7db0b6d8f89a5e04aafd
Duncan Mak <duncanmak@gmail.com>
2011-08-24 13:10:02 -0400

    Give magit-status a keybinding.

--- init.el
+++ init.el
@@ -69,6 +69,8 @@
 ;; Make all "yes or no" prompts show "y or n" instead
 (fset 'yes-or-no-p 'y-or-n-p)
 
+(global-set-key (kbd "C-x g") 'magit-status)
+
 (delete-selection-mode t)
 (show-paren-mode t)
 
