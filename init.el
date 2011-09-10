bdd32f1baf2bd583d7c0940aa4fdc46f348578cd
Duncan Mak <duncanmak@gmail.com>
2011-09-10 13:09:52 -0400

    Fix the loading of ibuffer-vc.

--- init.el
+++ init.el
@@ -37,23 +37,22 @@
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
-
-(require 'ibuffer-vc)
 (add-hook 'ibuffer-hook
 	  (lambda ()
 	    (ibuffer-vc-set-filter-groups-by-vc-root)
-	    (ibuffer-do-sort-by-alphabetic)))
-(setq ibuffer-formats
-      '((mark modified read-only vc-status-mini " "
-              (name 18 18 :left :elide)
-              " "
-              (size 9 -1 :right)
-              " "
-              (mode 16 16 :left :elide)
-              " "
-              (vc-status 16 16 :left)
-              " "
-              filename-and-process)))
+	    (ibuffer-do-sort-by-alphabetic)
+            (setq ibuffer-formats
+                  '((mark modified read-only vc-status-mini " "
+                          (name 18 18 :left :elide)
+                          " "
+                          (size 9 -1 :right)
+                          " "
+                          (mode 16 16 :left :elide)
+                          " "
+                          (vc-status 16 16 :left)
+                          " "
+                          filename-and-process)))
+            ))
 
 ;; Make control+pageup/down scroll the other buffer
 (global-set-key [C-next]  'scroll-other-window)
