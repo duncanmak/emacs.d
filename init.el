c2eb82f4e3d887f3bec5064ed73ba8f0b5953b33
Duncan Mak <duncanmak@gmail.com>
2011-09-18 03:02:11 -0400

    Try out ido.

--- init.el
+++ init.el
@@ -8,6 +8,21 @@
 
 (require 'sr-speedbar)
 
+;;; ido
+(require 'ido)                      ; ido is part of emacs 
+(ido-mode t)                        ; for both buffers and files
+(setq 
+   ido-ignore-buffers               ; ignore these guys
+   '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido")
+   ido-work-directory-list '("~/" "~/Desktop" "~/Documents")
+   ido-case-fold  t                 ; be case-insensitive
+   ido-use-filename-at-point nil    ; don't use filename at point (annoying)
+   ido-use-url-at-point nil         ;  don't use url at point (annoying)
+   ido-enable-flex-matching t       ; be flexible
+   ido-max-prospects 6              ; don't spam my minibuffer
+   ido-confirm-unique-completion t) ; wait for RET, even with unique completion
+
+
 ;;; Mac specific
 (when (eq system-type 'darwin)
   (require 'ls-lisp)
