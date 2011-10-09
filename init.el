d53ffce1dfbab1b8e6191b2b6aaa4369c5a0e1de
Duncan Mak <duncan@vbox.(none)>
2011-10-08 20:47:40 -0400

    Set c-annotation-face to something nicer.

--- init.el
+++ init.el
@@ -224,7 +224,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(c-annotation-face ((t (:foreground "gray"))) t))
 
 (require 'package)
 ;; Add the original Emacs Lisp Package Archive
