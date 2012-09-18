c326ff46565f022f850111d652ceda411faca4e9
Duncan Mak <duncan.mak@xamarin.com>
2012-09-18 14:34:07 -0400

    Fix up rcirc.

--- init.el
+++ init.el
@@ -276,36 +276,7 @@
 (add-hook 'scala-mode-hook 'scala-hook)
 
 ;;; rcirc
-(eval-after-load 'rcirc
-  '(defun-rcirc-command reconnect (arg)
-     "Reconnect the server process."
-     (interactive "i")
-     (unless process
-       (error "There's no process for this target"))
-     (let* ((server (car (process-contact process)))
-            (port (process-contact process :service))
-            (nick (rcirc-nick process))
-            (password (read-passwd "IRC Password: "))
-            channels query-buffers)
-       (dolist (buf (buffer-list))
-         (with-current-buffer buf
-           (when (eq process (rcirc-buffer-process))
-             (remove-hook 'change-major-mode-hook
-                          'rcirc-change-major-mode-hook)
-             (if (rcirc-channel-p rcirc-target)
-                 (setq channels (cons rcirc-target channels))
-               (setq query-buffers (cons buf query-buffers))))))
-       (delete-process process)
-       (rcirc-connect server port nick
-                      rcirc-default-user-name
-                      rcirc-default-user-full-name
-                      channels password))))
-
-(add-hook 'rcirc-mode-hook
-          (lambda ()
-            (rcirc-track-minor-mode 1)
-            (require 'arrange-buffers)
-            (define-key rcirc-mode-map (kbd "<f7>") '(lambda () (interactive) (arrange-buffers "^#" "rcirc-mode")))))
+(load-file (expand-file-name "~/.emacs.d/rcirc-additions.el"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
