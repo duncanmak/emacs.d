56811759ed6cff13d09daf5ad97d6e7f584e3a10
Duncan Mak <duncan.mak@xamarin.com>
2012-10-15 14:03:11 -0400

    Include golden-ratio.

--- init.el
+++ init.el
@@ -295,6 +295,10 @@
 ;;; rcirc
 (load-file (expand-file-name "~/.emacs.d/rcirc-additions.el"))
 
+;;; golden-ratio
+(require 'golden-ratio)
+(golden-ratio-enable)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
