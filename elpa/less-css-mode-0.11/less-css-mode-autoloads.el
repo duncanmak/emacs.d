374f315a8072e0e9f2fe22edd0d83e35a66d61d1
Duncan Mak <duncan.mak@xamarin.com>
2012-11-29 17:47:05 -0500

    Update less-css-mode

--- elpa/less-css-mode-0.11/less-css-mode-autoloads.el
+++ elpa/less-css-mode-0.11/less-css-mode-autoloads.el
@@ -4,7 +4,7 @@
 
 
 ;;;### (autoloads (flymake-less-css-init less-css-mode less-css-compile)
-;;;;;;  "less-css-mode" "less-css-mode.el" (20555 38800))
+;;;;;;  "less-css-mode" "less-css-mode.el" (20653 9026))
 ;;; Generated autoloads from less-css-mode.el
 
 (autoload 'less-css-compile "less-css-mode" "\
@@ -28,7 +28,7 @@ Flymake support for LESS files
 
 ;;;***
 
-;;;### (autoloads nil nil ("less-css-mode-pkg.el") (20555 38800 360373))
+;;;### (autoloads nil nil ("less-css-mode-pkg.el") (20653 9026 306840))
 
 ;;;***
 
