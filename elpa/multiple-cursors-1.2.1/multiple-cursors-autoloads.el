90f648683f456a0a44cefb272b122eb91a158755
Duncan Mak <duncan@a-chinaman.com>
2013-02-21 17:36:08 -0500

    Update multiple-cursors

--- elpa/multiple-cursors-1.2.1/multiple-cursors-autoloads.el
+++ elpa/multiple-cursors-1.2.1/multiple-cursors-autoloads.el
@@ -4,8 +4,8 @@
 
 
 ;;;### (autoloads (mc/edit-beginnings-of-lines mc/edit-ends-of-lines
-;;;;;;  mc/edit-lines) "mc-edit-lines" "mc-edit-lines.el" (20679
-;;;;;;  32359))
+;;;;;;  mc/edit-lines) "mc-edit-lines" "mc-edit-lines.el" (20771
+;;;;;;  3249))
 ;;; Generated autoloads from mc-edit-lines.el
 
 (autoload 'mc/edit-lines "mc-edit-lines" "\
@@ -36,7 +36,7 @@ Add one cursor to the beginning of each line in the active region.
 ;;;;;;  mc/mark-previous-word-like-this mc/mark-previous-like-this
 ;;;;;;  mc/mark-next-symbol-like-this mc/mark-next-word-like-this
 ;;;;;;  mc/mark-next-like-this) "mc-mark-more" "mc-mark-more.el"
-;;;;;;  (20679 32359))
+;;;;;;  (20771 3249))
 ;;; Generated autoloads from mc-mark-more.el
 
 (autoload 'mc/mark-next-like-this "mc-mark-more" "\
@@ -161,8 +161,30 @@ Mark the tag we're in and its pair for renaming.
 
 ;;;***
 
+;;;### (autoloads (mc/sort-regions mc/reverse-regions mc/insert-numbers)
+;;;;;;  "mc-separate-operations" "mc-separate-operations.el" (20771
+;;;;;;  3248))
+;;; Generated autoloads from mc-separate-operations.el
+
+(autoload 'mc/insert-numbers "mc-separate-operations" "\
+Insert increasing numbers for each cursor, starting at 0 or ARG.
+
+\(fn ARG)" t nil)
+
+(autoload 'mc/reverse-regions "mc-separate-operations" "\
+
+
+\(fn)" t nil)
+
+(autoload 'mc/sort-regions "mc-separate-operations" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
 ;;;### (autoloads (set-rectangular-region-anchor) "rectangular-region-mode"
-;;;;;;  "rectangular-region-mode.el" (20679 32358))
+;;;;;;  "rectangular-region-mode.el" (20771 3248))
 ;;; Generated autoloads from rectangular-region-mode.el
 
 (autoload 'set-rectangular-region-anchor "rectangular-region-mode" "\
@@ -176,8 +198,8 @@ an exceedingly quick way of adding multiple cursors to multiple lines.
 ;;;***
 
 ;;;### (autoloads nil nil ("mc-cycle-cursors.el" "multiple-cursors-core.el"
-;;;;;;  "multiple-cursors-pkg.el" "multiple-cursors.el") (20679 32359
-;;;;;;  109810))
+;;;;;;  "multiple-cursors-pkg.el" "multiple-cursors.el") (20771 3249
+;;;;;;  157134))
 
 ;;;***
 
