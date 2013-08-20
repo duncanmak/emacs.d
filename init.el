be59cb07ad656ab6e47003e07a7bb541b2ca0ae6
Duncan Mak <duncan.mak@xamarin.com>
2013-08-20 18:51:06 -0400

    Turn off ibuffer-auto-mode. It makes things really slow.

--- init.el
+++ init.el
@@ -154,7 +154,6 @@
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 (add-hook 'ibuffer-hook
 	  (lambda ()
-            (ibuffer-auto-mode 1)
 	    (ibuffer-vc-set-filter-groups-by-vc-root)
 	    (ibuffer-do-sort-by-alphabetic)))
 
