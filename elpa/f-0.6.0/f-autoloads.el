f8218b9c82dedc9ac9e8302025ac66acda196bfe
Duncan Mak <duncan@a-chinaman.com>
2013-08-27 17:15:32 -0400

    Update packages

---
+++ elpa/f-0.6.0/f-autoloads.el
@@ -0,0 +1,18 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21021 2194 353875
+;;;;;;  0))
+
+;;;***
+
+(provide 'f-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; f-autoloads.el ends here
