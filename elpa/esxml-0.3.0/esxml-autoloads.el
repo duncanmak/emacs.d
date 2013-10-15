9317904caf8b70f066f6cf051997173c2e11cd93
Duncan Mak <duncan@a-chinaman.com>
2013-10-15 19:28:53 -0400

    Update packages

---
+++ elpa/esxml-0.3.0/esxml-autoloads.el
@@ -0,0 +1,18 @@
+;;; esxml-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("esxml-form.el" "esxml-html.el" "esxml-pkg.el"
+;;;;;;  "esxml.el") (21021 13328 145278 0))
+
+;;;***
+
+(provide 'esxml-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; esxml-autoloads.el ends here
