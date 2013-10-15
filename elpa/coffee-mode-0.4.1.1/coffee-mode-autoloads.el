9060344ab6821a14a2eee18e4b3bca3f667b060a
Duncan Mak <duncan@a-chinaman.com>
2013-10-15 19:24:18 -0400

    Update coffee-mode

--- elpa/coffee-mode-0.4.1.1/coffee-mode-autoloads.el
+++ elpa/coffee-mode-0.4.1.1/coffee-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 
-;;;### (autoloads (coffee-mode) "coffee-mode" "coffee-mode.el" (20499
-;;;;;;  36129))
+;;;### (autoloads (coffee-mode) "coffee-mode" "coffee-mode.el" (21085
+;;;;;;  52825 0 0))
 ;;; Generated autoloads from coffee-mode.el
 
 (autoload 'coffee-mode "coffee-mode" "\
@@ -12,13 +12,14 @@ Major mode for editing CoffeeScript.
 
 \(fn)" t nil)
 
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
 
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
 ;;;***
 
-;;;### (autoloads nil nil ("coffee-mode-pkg.el") (20499 36129 920438))
+;;;### (autoloads nil nil ("coffee-mode-pkg.el") (21085 52825 603953
+;;;;;;  0))
 
 ;;;***
 
